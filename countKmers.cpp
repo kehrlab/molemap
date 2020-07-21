645d5d6508d057cc961fc71997f796ea5dd85d4c
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2020-07-21 13:17:57 +0200

    Add files via upload

--- countKmers.cpp
+++ countKmers.cpp
@@ -11,9 +11,9 @@ g++ countKmers.cpp -o countK
 */
 std::pair <unsigned,unsigned> hashkMer(const DnaString & kmer, const unsigned k);
 std::pair <unsigned,unsigned> rollinghashkMer(unsigned & oldHash, unsigned & oldHash2, const Dna & newnuc, const unsigned k);
-unsigned  GetBkt(const unsigned & hash, const std::vector<unsigned> & C, const unsigned long long bucket_number);
-unsigned  ReqBkt(const unsigned & hash, std::vector<unsigned> & C, const unsigned long long bucket_number);
-std::vector<unsigned> RetPos(const DnaString & kmer, const std::vector<unsigned> & C,const std::vector<unsigned> & dir,const std::vector<unsigned> & pos, const unsigned long long bucket_number);
+unsigned  GetBkt(const unsigned & hash, const String<unsigned> & C, const unsigned long long bucket_number);
+unsigned  ReqBkt(const unsigned & hash, String<unsigned> & C, const unsigned long long bucket_number);
+std::vector<std::pair <unsigned,unsigned>> RetPos(const DnaString & kmer, const String<unsigned> & C,const String<unsigned> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number);
 
 int main(int argc, char *argv[]){
 
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]){
 
   // for (int a=0;a<100;a++){
 
-  if(argc!=4){
+  if(argc!=5){
     std::cerr << "Usage: ./countK InputFILE k bucket_number \n\n";
     exit(-1);
   }
@@ -46,166 +46,143 @@ int main(int argc, char *argv[]){
   }
 
   std::cerr << "Genome read in. \n";
-
   // defining key parameters
 
   unsigned k=std::stoi(argv[2]); // length of k-mer
 
   unsigned long long bucket_number=std::stoll(argv[3]); // should depend on k and the length of the indexed sequence
 
-  // concatination of all sequences
+  // choosing Chromosome
 
-  Dna5String seq=seqs[36];
-
-  std::cerr << "ID: " << ids[36] << "\n";
-
-  std::cerr << "Chromosome lengths: " << length(seq) << "\n";
+  // Dna5String seq=seqs[36];
+  //
+  // std::cerr << "ID: " << ids[36] << "\n";
+  //
+  // std::cerr << "Chromosome lengths: " << length(seq) << "\n";
 
   // building index storage
 
-  std::vector<unsigned> dir(bucket_number,0);       // pow(4,k) depending on k-mer size
-  std::vector<unsigned> pos(length(seq),0);         // length(seq)-k+1 runns into error
-  std::vector<unsigned> C(bucket_number,-1);
+  String<unsigned> dir;
+  resize(dir,bucket_number,0);
+  String<std::pair <unsigned,unsigned>> pos;
+  resize(pos,length(concat(seqs)));
+  String<unsigned> C;
+  resize(C,bucket_number,-1);
+
+  typedef Iterator<String<unsigned>>::Type Titrs;
+
   std::vector<unsigned>::iterator itrv;
   std::vector<unsigned>::reverse_iterator itrvr;
 
-  std::cerr << "Index prepared. \n";
-  // counting k-mers
-
-  std::pair<unsigned, unsigned> hash=hashkMer(infix(seq,0,k),k);    // calculation of the hash value for the first k-mer
   unsigned long long c;
+  unsigned CHROM =0;
 
-  for (unsigned i = 0;i<length(seq)-k;++i){
-    if (i%10000==0){
-        std::cerr << i << "\n";
+  std::cerr << "Index prepared. \n";
+
+  // iterating over the stringSet (Chromosomes)
+  typedef Iterator<StringSet<Dna5String> >::Type TStringSetIterator;
+  for (TStringSetIterator seq = begin(seqs); seq != end(seqs); ++seq){
+
+    // counting k-mers
+
+    std::pair<unsigned, unsigned> hash=hashkMer(infix(*seq,0,k),k);    // calculation of the hash value for the first k-mer
+
+
+    for (unsigned i = 0;i<length(*seq)-k;++i){
+      c=ReqBkt(std::min(hash.first,hash.second),C,bucket_number);     // indexing the hashed k-mers
+      dir[c+1]+=1;
+      if ((*seq)[i+k]!='N'){                                             // calculation of the hash value for the next k-mer
+        hash=rollinghashkMer(hash.first,hash.second,(*seq)[i+k],k);
+      }
+      else {                                                          // reinitialization of the hashvalue after encountering an "N"
+        i+=k+1;
+        hash=hashkMer(infix(*seq,i,i+k),k);
+      }
     }
-    c=ReqBkt(std::min(hash.first,hash.second),C,bucket_number);     // indexing the hashed k-mers
+    c=ReqBkt(std::min(hash.first,hash.second),C,bucket_number);       // indexing of the last element
     dir[c+1]+=1;
-    if (seq[i+k]!='N'){                                             // calculation of the hash value for the next k-mer
-      hash=rollinghashkMer(hash.first,hash.second,seq[i+k],k);
-    }
-    else {                                                          // reinitialization of the hashvalue after encountering an "N"
-      i+=k+1;
-      hash=hashkMer(infix(seq,i,i+k),k);
-    }
+
   }
-  c=ReqBkt(std::min(hash.first,hash.second),C,bucket_number);       // indexing of the last element
-  dir[c+1]+=1;
 
   std::cerr << "Index initially filled. \n";
 
-  unsigned sum=length(seq)-k+1;
-
-
   // cumulative sum
+
+  unsigned sum=length(concat(seqs))-k+1;
   std::vector<unsigned> abundance; //tracking k-mer abundances
 
-  for (itrvr=dir.rbegin();itrvr!=dir.rend();++itrvr){
-    if (*itrvr!=0){   //tracking k-mer abundances
-    sum-=*itrvr;
-    abundance.push_back(*itrvr);} //tracking k-mer abundances
-    *itrvr=sum;
+  for (Titrs itrs=end(dir)-1;itrs!=begin(dir)-1;--itrs){
+    if (*itrs!=0){   //tracking k-mer abundances
+      sum-=*itrs;
+    abundance.push_back(*itrs);} //tracking k-mer abundances
+    *itrs=sum;
   }
 
   std::cerr << "cumulated sum culculation finished. \n";
 
-  // filling pos
+  // iterating over the stringSet (Chromosomes)
 
-  hash=hashkMer(infix(seq,0,k),k);                                // calculation of the hash value for the first k-mer
+  for (TStringSetIterator seq = begin(seqs); seq != end(seqs); ++seq){
 
-  for (unsigned i = 0;i<length(seq)-k;++i){
-    if (i%10000==0){
-        std::cerr << i << "\n";
+    // filling pos
+
+    std::pair<unsigned, unsigned> hash=hashkMer(infix(*seq,0,k),k);                                // calculation of the hash value for the first k-mer
+
+    for (unsigned i = 0;i<length(*seq)-k;++i){
+      c=GetBkt(std::min(hash.first,hash.second),C,bucket_number);   // filling of the position table
+      pos[dir[c+1]]=std::make_pair(CHROM,i);
+      dir[c+1]++;
+      if ((*seq)[i+k]!='N'){                                           // calculation of the hash value for the next k-mer
+        hash=rollinghashkMer(hash.first,hash.second,(*seq)[i+k],k);
+      }
+      else {                                                        // reinitialization of the hashvalue after encountering an "N"
+        i+=k+1;
+        hash=hashkMer(infix(*seq,i,i+k),k);
+      }
     }
-    c=GetBkt(std::min(hash.first,hash.second),C,bucket_number);   // filling of the position table
-    pos[dir[c+1]]=i;
+    c=GetBkt(std::min(hash.first,hash.second),C,bucket_number);     // filling the position table for the last element
+    pos[dir[c+1]]=std::make_pair(CHROM,length(*seq)-k);
     dir[c+1]++;
-    if (seq[i+k]!='N'){                                           // calculation of the hash value for the next k-mer
-      hash=rollinghashkMer(hash.first,hash.second,seq[i+k],k);
-    }
-    else {                                                        // reinitialization of the hashvalue after encountering an "N"
-      i+=k+1;
-      hash=hashkMer(infix(seq,i,i+k),k);
-    }
+    CHROM++;
   }
-  c=GetBkt(std::min(hash.first,hash.second),C,bucket_number);     // filling the position table for the last element
-  pos[dir[c+1]]=length(seq)-k;
-  dir[c+1]++;
-
 
   std::cerr << "Index build. \n";
 
   // write index to file
-  // std::ofstream index;
-  // index.open("index.txt");
-  //
-  // index << bucket_number << " " << k << " " << length(seq) << "\n";
-  //
-  // index << "\n";
-  // for (itrv=C.begin();itrv!=C.end();itrv++){
-  //   index << *itrv << " ";
-  // }
-  // index << "\n";
-  //
-  // index << "\n";
-  // for (itrv=dir.begin();itrv!=dir.end();itrv++){
-  //   index << *itrv << " ";
-  // }
-  // index << "\n";
-  //
-  // index << "\n";
-  // for (itrv=pos.begin();itrv!=pos.end();itrv++){
-  //   index << *itrv << " ";
-  // }
-  // index << "\n";
-  // index.close();
 
-  // calculating abundances of k-k_mers
+  String<std::pair <unsigned,unsigned>, External<ExternalConfigLarge<>> > extpos;
+  if (!open(extpos, "index_pos.txt", OPEN_WRONLY | OPEN_CREATE)){
+    throw std::runtime_error("Could not open index counts file." );
+  }
+  assign(extpos, pos, Exact());
+  close(extpos);
 
-  std::sort(abundance.rbegin(),abundance.rend());
+  String<unsigned, External<> > extdir;
+  if (!open(extdir, "index_dir.txt", OPEN_WRONLY | OPEN_CREATE)){
+    throw std::runtime_error("Could not open index counts file." );
+  }
+  assign(extdir, dir, Exact());
+  close(extdir);
 
-  std::cerr <<  "abundances sorted.\n";
+  String<unsigned, External<> > extC;
+  if (!open(extC, "index_C.txt", OPEN_WRONLY | OPEN_CREATE)){
+    throw std::runtime_error("Could not open index counts file." );
+  }
+  assign(extC, C, Exact());
+  close(extC);
 
-  // for (itrv=abundance.begin();itrv!=abundance.end();itrv++){
-  //   std::cout<<*itrv<<" ";
-  // }
-
-  // writing abundances to file
-  //
-  // String<Dna, External<> > myLargeGenome;
-  // if (!open(extAbundance, "abundances.txt", OPEN_WRONLY | OPEN_CREATE)){
-  //   throw std::runtime_error("Could not open index counts file." );
-  // }
-  // assign(extAbundance, abundance, Exact());
-
-
-
-
-
-
-
-//
-    //
-    std::ofstream abund;
-    abund.open("abundances.txt");
-
-    for (itrv=abundance.begin();itrv!=abundance.end();itrv++){
-      abund<<*itrv<<" ";
-    }
-
-    abund.close();
-
-  std::cerr <<  "abundances writen to file.\n";
+  std::cerr << "Index writen to file.\n";
 
   // Kontrollausgabe
 
-  //
-  // DnaString testDNA=argv[4];
-  // std::vector<unsigned> positions=RetPos(testDNA,C,dir,pos,bucket_number);
-  // for (itrv=positions.begin();itrv!=positions.end();itrv++){
-  //   std::cout << *itrv <<" ";
-  // }
-  // std::cout << "\n";
+
+  DnaString testDNA=argv[4];
+  std::vector<std::pair <unsigned,unsigned>> positions=RetPos(testDNA,C,dir,pos,bucket_number);
+  std::vector<std::pair <unsigned,unsigned>>::iterator itrpv;
+  for (itrpv=positions.begin();itrpv!=positions.end();itrpv++){
+    std::cout << (*itrpv).first << " " << (*itrpv).second <<"\n";
+  }
+  std::cout << "\n";
 
 // }
 
@@ -215,8 +192,8 @@ int main(int argc, char *argv[]){
 }
 
 // return k-mer positions
-std::vector<unsigned> RetPos(const DnaString & kmer, const std::vector<unsigned> & C,const std::vector<unsigned> & dir,const std::vector<unsigned> & pos, const unsigned long long bucket_number){
-      std::vector<unsigned> positions;
+std::vector<std::pair <unsigned,unsigned>> RetPos(const DnaString & kmer, const String<unsigned> & C,const String<unsigned> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number){
+      std::vector<std::pair <unsigned,unsigned>> positions;
       std::pair <unsigned,unsigned> hash=hashkMer(kmer,length(kmer));
       int c=GetBkt(std::min(hash.first,hash.second),C,bucket_number);
       for (unsigned i = dir[c];i!=dir[c+1];i++){
@@ -226,7 +203,7 @@ std::vector<unsigned> RetPos(const DnaString & kmer, const std::vector<unsigned>
 }
 
 // Find correct Bucket
-unsigned  GetBkt(const unsigned & hash, const std::vector<unsigned> & C, const unsigned long long bucket_number){
+unsigned  GetBkt(const unsigned & hash, const String<unsigned> & C, const unsigned long long bucket_number){
   std::srand(hash);
   unsigned long long i=std::rand()%bucket_number;
   unsigned d=0;
@@ -243,7 +220,7 @@ unsigned  GetBkt(const unsigned & hash, const std::vector<unsigned> & C, const u
 }
 
 // Request a Bucket
-unsigned  ReqBkt(const unsigned & hash, std::vector<unsigned> & C, const unsigned long long bucket_number){
+unsigned  ReqBkt(const unsigned & hash, String<unsigned> & C, const unsigned long long bucket_number){
   unsigned long long i = GetBkt(hash,C,bucket_number);
   C[i]=hash;
   return i;
