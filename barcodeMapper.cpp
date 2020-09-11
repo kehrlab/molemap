fd7d032aaf1338fb2910d1509ef3d45ea1aaa323
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2020-09-11 09:24:22 +0200

    Add files via upload

--- barcodeMapper.cpp
+++ barcodeMapper.cpp
@@ -116,10 +116,8 @@ sort(kmer_list.begin(),kmer_list.end());
 
 // iterate over k-mers (sliding window)
 
-std::cerr << "\nbefore log building.";
 float lookLog[100]= {0,1,0.693147,1.09861,1.38629,1.60944,1.79176,1.94591,2.07944,2.19722,2.30259,2.3979,2.48491,2.56495,2.63906,2.70805,2.77259,2.83321,2.89037,2.94444,2.99573,3.04452,3.09104,3.13549,3.17805,3.21888,3.2581,3.29584,3.3322,3.3673,3.4012,3.43399,3.46574,3.49651,3.52636,3.55535,3.58352,3.61092,3.63759,3.66356,3.68888,3.71357,3.73767,3.7612,3.78419,3.80666,3.82864,3.85015,3.8712,3.89182,3.91202,3.93183,3.95124,3.97029,3.98898,4.00733,4.02535,4.04305,4.06044,4.07754,4.09434,4.11087,4.12713,4.14313,4.15888,4.17439,4.18965,4.20469,4.21951,4.23411,4.2485,4.26268,4.27667,4.29046,4.30407,4.31749,4.33073,4.34381,4.35671,4.36945,4.38203,4.39445,4.40672,4.41884,4.43082,4.44265,4.45435,4.46591,4.47734,4.48864,4.49981,4.51086,4.52179,4.5326,4.54329,4.55388,4.56435,4.57471,4.58497,4.59512};
-std::cerr << "\nafter log building.";
-std::cerr << "\n array size: " << sizeof(lookLog)/sizeof(lookLog[0]);
+
 #define REF(X) std::get<0>(*(X))
 #define POS(X) std::get<1>(*(X))
 #define ABU(X) std::get<2>(*(X))
@@ -134,7 +132,6 @@ if (ABU(kmer_list.begin())==1){                                 // updating wind
   window_quality+=1/lookLog[ABU(kmer_list.begin())];
 }
 
-std::cerr << "\nafter first log usage.\n";
 
 std::vector<std::tuple<double,unsigned,unsigned>> best_windows(window_count,std::make_tuple(0,0,0)); //(maping_quality, reference, position in referende)
 std::vector<std::tuple<double,unsigned,unsigned>>::iterator itrbw;
@@ -174,8 +171,14 @@ for(itrk=kmer_list.begin()+1;itrk!=kmer_list.end();itrk++){ // iterating over km
        for (itrbw=best_windows.begin();itrbw!=best_windows.end();itrbw++){                             // iterate over best_windows
          if (std::get<1>(*itrbw)==REF(itrk) && abs((int)POS(itrk)-(int)std::get<2>(*itrbw))<=window_size){ // if overlapping window: keep better window and break loop.
            if (window_quality > std::get<0>(*itrbw)){
+             if(POS(itrk)<2000){
+               std::cerr << "before: "<< std::get<3>(*itrbw);
+             }
              *itrbw=std::make_tuple(window_quality, REF(itrk), POS(itrk));
              sort(best_windows.begin(),best_windows.end());
+             if(POS(itrk)<2000){
+               std::cerr << " after: "<< std::get<3>(*itrbw) << "\n";
+             }
              // best_windows.erase(itrbw);
              // for (itrbw2=best_windows.begin()+1;itrbw2!=best_windows.end();itrbw2++){                             // iterate over best_windows
              //    if(window_quality < std::get<0>(*itrbw2)){                                                       // if (as soon as) quality is worse than quality in best_windows
