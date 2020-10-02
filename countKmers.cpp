e906232c691874e345717ca1aa00c4321f866867
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2020-10-02 15:32:03 +0200

    Add files via upload

--- countKmers.cpp
+++ countKmers.cpp
@@ -173,7 +173,7 @@ int main(int argc, char *argv[]){
 
 
   DnaString testDNA="TTTGGCCT";
-  std::vector<std::pair <unsigned,unsigned>> positions=RetPos(testDNA,C,dir,pos,bucket_number);
+  std::vector<std::pair <unsigned,unsigned>> positions=RetPos(hashkMer(testDNA,k),C,dir,pos,bucket_number);
   std::vector<std::pair <unsigned,unsigned>>::iterator itrpv;
   for (itrpv=positions.begin();itrpv!=positions.end();itrpv++){
     std::cout << (*itrpv).first << " " << (*itrpv).second <<"\n";
