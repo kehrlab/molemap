1875bcde38adaa7086bd6ee34f21a66489efc525
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2020-10-08 13:56:26 +0200

    Add files via upload

--- src/functions.h
+++ src/functions.h
@@ -7,7 +7,7 @@
 using namespace seqan;
 
 std::pair <unsigned long long,unsigned long long> hashkMer(const DnaString & kmer, const unsigned k);
-std::pair <unsigned long long,unsigned long long> rollinghashkMer(unsigned long long & oldHash, unsigned long long & oldHash2, const Dna & newnuc, const unsigned k);
+std::pair <unsigned long long,unsigned long long> rollinghashkMer(long long int & oldHash, long long int & oldHash2, const Dna & newnuc, const unsigned k);
 unsigned long long GetBkt(const unsigned long long & hash, const String<unsigned> & C, const unsigned long long bucket_number);
 unsigned  long long ReqBkt(const unsigned long long & hash, String<unsigned> & C, const unsigned long long bucket_number);
 std::vector<std::pair <unsigned,unsigned>> RetPos(const unsigned long long & hash, const String<unsigned> & C,const String<unsigned> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number);
