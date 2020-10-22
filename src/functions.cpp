0c82b067c2d9ca555f0b9c8a4b613d80105ac14d
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2020-10-22 13:39:09 +0200

    Add files via upload

--- src/functions.cpp
+++ src/functions.cpp
@@ -6,7 +6,7 @@
 using namespace seqan;
 
 //Insert k-mer positions into vector in sorted order
-void AppendPos(std::vector<std::tuple <unsigned,unsigned,unsigned>> & kmer_list, const long long int & hash, const String<unsigned long long> & C,const String<unsigned long long> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number){
+void AppendPos(std::vector<std::tuple <unsigned,unsigned,unsigned>> & kmer_list, const long long int & hash, const String<int long long> & C,const String<unsigned long long> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number){
       int c=GetBkt(hash,C,bucket_number);
       unsigned abundance=dir[c+1]-dir[c];
       kmer_list.reserve(kmer_list.size()+abundance);
@@ -16,7 +16,7 @@ void AppendPos(std::vector<std::tuple <unsigned,unsigned,unsigned>> & kmer_list,
       return;
 }
 // return k-mer positions
-std::vector<std::pair <unsigned,unsigned>> RetPos(const long long int & hash, const String<unsigned long long> & C,const String<unsigned long long> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number){
+std::vector<std::pair <unsigned,unsigned>> RetPos(const long long int & hash, const String<int long long> & C,const String<unsigned long long> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number){
       std::vector<std::pair <unsigned,unsigned>> positions;
       int c=GetBkt(hash,C,bucket_number);
       for (unsigned i = dir[c];i!=dir[c+1];i++){
@@ -26,7 +26,7 @@ std::vector<std::pair <unsigned,unsigned>> RetPos(const long long int & hash, co
 }
 
 // Find correct Bucket
-unsigned long long  GetBkt(const long long int & hash, const String<unsigned long long> & C, const unsigned long long bucket_number){
+unsigned long long  GetBkt(const long long int & hash, const String<int long long> & C, const unsigned long long bucket_number){
   std::srand(hash);
   unsigned long long i=std::rand()%bucket_number;
   unsigned d=0;
@@ -43,7 +43,7 @@ unsigned long long  GetBkt(const long long int & hash, const String<unsigned lon
 }
 
 // Request a Bucket
-unsigned  long long ReqBkt(const long long int & hash, String<unsigned long long> & C, const unsigned long long bucket_number){
+unsigned  long long ReqBkt(const long long int & hash, String<int long long> & C, const unsigned long long bucket_number){
   unsigned long long i = GetBkt(hash,C,bucket_number);
   C[i]=hash;
   return i;
--- src/functions.h
+++ src/functions.h
@@ -6,11 +6,11 @@
 # include <iostream>
 using namespace seqan;
 
-void AppendPos(std::vector<std::tuple <unsigned,unsigned,unsigned>> & kmer_list, const long long int & hash, const String<unsigned long long> & C,const String<unsigned long long> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number);
+void AppendPos(std::vector<std::tuple <unsigned,unsigned,unsigned>> & kmer_list, const long long int & hash, const String<int long long> & C,const String<unsigned long long> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number);
 std::pair <long long int,long long int> hashkMer(const DnaString & kmer, const unsigned k);
 void rollinghashkMer(long long int & oldHash, long long int & oldHash2, const Dna & newnuc, const unsigned k, const long long int & maxhash);
-unsigned long long GetBkt(const long long int & hash, const String<unsigned long long> & C, const unsigned long long bucket_number);
-unsigned  long long ReqBkt(const long long int & hash, String<unsigned long long> & C, const unsigned long long bucket_number);
-std::vector<std::pair <unsigned,unsigned>> RetPos(const long long int & hash, const String<unsigned long long> & C,const String<unsigned long long> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number);
+unsigned long long GetBkt(const long long int & hash, const String<int long long> & C, const unsigned long long bucket_number);
+unsigned  long long ReqBkt(const long long int & hash, String<int long long> & C, const unsigned long long bucket_number);
+std::vector<std::pair <unsigned,unsigned>> RetPos(const long long int & hash, const String<int long long> & C,const String<unsigned long long> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number);
 
 #endif
