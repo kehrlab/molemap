2fd3520fe3cc476cf8169fe9f2d2a3ac5680df37
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-01-19 14:19:53 +0100

    Update functions.h

--- src/functions.h
+++ src/functions.h
@@ -7,7 +7,7 @@
 using namespace seqan;
 
 long long int ReturnSmaller(const long long int hash1,const long long int hash2,const long long int random_seed);
-long long int InitMini(const DnaString & string, const unsigned k, std::pair <long long int, long long int> hash, const long long int & maxhash,const long long int random_seed, long long int & minimizer_position);
+long long int InitMini(const DnaString & string, const unsigned k, std::pair <long long int, long long int> & hash, const long long int & maxhash,const long long int random_seed, long long int & minimizer_position);
 int RollMini(long long int & minimizer, std::pair <long long int, long long int> & hash, const Dna & newnuc, const unsigned k, const long long int & maxhash,const long long int random_seed);
 void AppendPos(std::vector<std::tuple <unsigned,unsigned,unsigned>> & kmer_list, const long long int & hash, const String<int long long> & C,const String<unsigned long long> & dir,const String<std::pair <unsigned,unsigned>> & pos, const unsigned long long bucket_number);
 std::pair <long long int,long long int> hashkMer(const DnaString & kmer, const unsigned k);
