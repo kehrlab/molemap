c0130af886cf1e7f160ebe0d896e9942bc648eff
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-02-25 13:28:33 +0100

    Add files via upload

--- barcodeMapper_minimize_dyn.cpp
+++ barcodeMapper_minimize_dyn.cpp
@@ -274,7 +274,7 @@ std::cout <<"\ntime: "<< (float)std::chrono::duration_cast<std::chrono::millisec
 
 
 for(itrbw=best_windows.begin();itrbw!=best_windows.end(); itrbw++){
-  std::cout<<"\nquality: " << (to_string(std::get<0>(*itrbw))+(" "*(to_string(std::get<0>(*itrbw)).length()-20))) << "\tref: " << std::get<1>(*itrbw)<< "\tstart: "<< std::get<2>(*itrbw) << "\tend: " << std::get<3>(*itrbw) << "\tlength: " << std::get<3>(*itrbw)-std::get<2>(*itrbw);
+  std::cout<<"\nquality: " << (to_string(std::get<0>(*itrbw))+(" "*(20-to_string(std::get<0>(*itrbw)).length()))) << "\tref: " << std::get<1>(*itrbw)<< "\tstart: "<< std::get<2>(*itrbw) << "\tend: " << std::get<3>(*itrbw) << "\tlength: " << std::get<3>(*itrbw)-std::get<2>(*itrbw);
 }
 std::cerr<<"\n";
 
