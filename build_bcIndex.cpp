1bde264919800df49e4961d8ccd652ebf646938f
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-03-04 14:32:47 +0100

    Add files via upload

--- build_bcIndex.cpp
+++ build_bcIndex.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]){
   file2.open(filename2,std::ios::in);
 
   getline(file1,id);
-  barcode=id.substr(id.find("RX:Z:")+5,16);
+  old_barcode=id.substr(id.find("RX:Z:")+5,16);
   // file1.tellg()
   std::cerr << "barcode: " << new_barcode << "\n";
 
@@ -40,9 +40,9 @@ int main(int argc, char *argv[]){
       std::cerr << "barcode: " << new_barcode << "\n";
       old_barcode=new_barcode;
     }
-    file1.ignore(std::numeric_limits<streamsize>::max(),"\n");
-    file1.ignore(std::numeric_limits<streamsize>::max(),"\n");
-    file1.ignore(std::numeric_limits<streamsize>::max(),"\n");
+    file1.ignore(100000,"\n");
+    file1.ignore(100000,"\n");
+    file1.ignore(100000,"\n");
 
 
   }
