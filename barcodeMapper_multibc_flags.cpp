14000031e0d4bbef0be27c88a6e7b4144cfe3e41
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-03-22 14:51:49 +0100

    Add files via upload

--- barcodeMapper_multibc_flags.cpp
+++ barcodeMapper_multibc_flags.cpp
@@ -391,16 +391,19 @@ void MapKmerList(std::vector<std::tuple<char,unsigned,unsigned,unsigned>> & kmer
     for (int i=(toshort.size()-1);i>=0;i--) {
       best_windows.erase(best_windows.begin()+toshort[i]);
     }
-    // std::cerr << "\n" << __LINE__;
+    std::cerr << "\n" << __LINE__;
 
     //filter low quality windows
     double qualityThreshold=20;
     if (std::get<0>(*(best_windows.end()-1))!=0) {
+      std::cerr << "\n" << __LINE__;
       while(std::get<0>(*best_windows.begin())<qualityThreshold){
+        std::cerr << "\n" << __LINE__;
         best_windows.erase(best_windows.begin());
+        std::cerr << "\n" << __LINE__;
       }
     }
-    // std::cerr << "\n" << __LINE__;
+    std::cerr << "\n" << __LINE__;
 
 
     // std::cerr<<"best_windows found. ";
