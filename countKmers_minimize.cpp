6a13123061b9c10fa54c2dc6f302720e5e4441f5
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-03-22 17:32:36 +0100

    Add files via upload

--- countKmers_minimize.cpp
+++ countKmers_minimize.cpp
@@ -92,7 +92,7 @@ int main(int argc, char *argv[]){
   // iterating over the stringSet (Chromosomes)
   typedef Iterator<StringSet<Dna5String> >::Type TStringSetIterator;
   for (TStringSetIterator seq = begin(seqs); seq != end(seqs); ++seq){
-    std::cerr << "Chrom: " << CHROM << "\n";
+    std::cerr << "Chrom: " << (int)CHROM << "\n";
     // counting k-mers
 
     std::pair<long long int, long long int> hash=hashkMer(infix(*seq,0,k),k);    // calculation of the hash value for the first k-mer
