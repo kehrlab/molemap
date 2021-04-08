0278dc18679baf80f227ea65bf23b9cc7097bfc5
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-04-08 10:05:19 +0200

    Add files via upload

--- barcodeMapper_multithread.cpp
+++ barcodeMapper_multithread.cpp
@@ -331,7 +331,7 @@ std::streampos BCI_pos1;
 std::streampos BCI_pos2;
 
 std::cerr << "Processing read file...";
-std::cerr << __LINE__ << "\n";
+
 kmer_list_struct_t kmer_list_struct_template; // filling template struct with constant parameters
 resize(kmer_list_struct_template.reads,2,Exact());
 std::vector<std::tuple<uint_fast8_t,uint32_t,uint32_t,uint32_t>> kmer_list;
@@ -343,36 +343,28 @@ kmer_list_struct_template.maxhash=maxhash;
 kmer_list_struct_template.random_seed=random_seed;
 kmer_list_struct_template.mini_window_size=mini_window_size;
 kmer_list_struct_template.bucket_number=bucket_number;
-std::cerr << __LINE__ << "\n";
 
 uint_fast8_t thread=0;                        //currently selected thread
 uint_fast8_t thread_count=3;                  //number of used threads on top of main thread
 pthread_t list_thread[thread_count];          //thread for creating kmer_list
-std::vector<bool> active_threads;
+std::vector<bool> active_threads;             //info about started threads
 resize(active_threads,thread_count,false);
-std::vector<kmer_list_struct_t> kmer_list_structs;
+std::vector<kmer_list_struct_t> kmer_list_structs; // input structs for threads
 resize(kmer_list_structs,thread_count,kmer_list_struct_template);
 
 while (atEnd(file1)!=1) { // proceeding through files
-  std::cerr << __LINE__ << "\n";
 
   BCI_pos1=file1.stream.file.tellg();
-  std::cerr << __LINE__ << "\n";
   readRecord(id1, read1, file1);
-  std::cerr << __LINE__ << "\n";
   assignValue(kmer_list_structs[thread].reads,0,read1);
-  std::cerr << __LINE__ << "\n";
   meta=toCString(id1);
-  std::cerr << __LINE__ << "\n";
   new_barcode=meta.substr(meta.find("RX:Z:")+5,16);
-  std::cerr << __LINE__ << "\n";
 
   if (barcode!=new_barcode){ //If Barcode changes: map kmer_list and reinitialize kmer_list
     //append Barcode Index
     BCI_pos2=file2.stream.file.tellg();
     BCI_barcodes.push_back(new_barcode);
     BCI_positions.push_back(std::make_pair(BCI_pos1,BCI_pos2));
-    std::cerr << __LINE__ << "\n";
 
     // map barcode and clear k_mer list
     // map barcode as soon as all k-mer mapping threads are finished
@@ -381,31 +373,24 @@ while (atEnd(file1)!=1) { // proceeding through files
         pthread_join(list_thread[i],NULL);
         active_threads[i]=false;
       }
-      std::cerr << __LINE__ << "\n";
     }
     if (!(kmer_list).empty()) {
-      std::cerr << __LINE__ << "\n";
       sort(kmer_list.begin(),kmer_list.end());
       MapKmerList(kmer_list,max_window_size,max_gap_size,window_count,toCString(options.output_file),barcode, options.q, options.l);
       kmer_list.clear();
-      std::cerr << __LINE__ << "\n";
 
     }
   }
-  std::cerr << __LINE__ << "\n";
   readRecord(id2, read2, file2);
   assignValue(kmer_list_structs[thread].reads,1,read2);
   barcode=new_barcode;
 
   //start new thread here
-  std::cerr << "thread: " << (int)thread << "\n";
   if (active_threads[thread]==true) {
-    std::cerr << __LINE__ << "\n";
 
     pthread_join(list_thread[thread],NULL);
     active_threads[thread]=false;
   }
-  std::cerr << __LINE__ << "\n";
 
   ret =  pthread_create(&list_thread[thread], NULL, &fillList, &kmer_list_structs[thread]);
   if(ret != 0) {
@@ -414,7 +399,6 @@ while (atEnd(file1)!=1) { // proceeding through files
   }
   active_threads[thread]=true;
   thread=(thread+1)%(thread_count);
-  std::cerr << __LINE__ << "\n";
 
 
   // for (TStringSetIterator it = begin(reads); it!=end(reads); ++it){                                            // Iterating over the reads
