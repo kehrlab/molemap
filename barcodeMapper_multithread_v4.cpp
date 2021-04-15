0fdb905ec75d8d4718a9d1cc3d4e3a39992a373f
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-04-15 17:10:33 +0200

    Add files via upload

--- barcodeMapper_multithread_v4.cpp
+++ barcodeMapper_multithread_v4.cpp
@@ -486,9 +486,9 @@ while (std::getline(file1,meta)) { // proceeding through files
       std::cerr << __LINE__<<"\n";
 
       kmer_list_structs[thread].BCI=std::vector<std::pair<std::streampos,std::streampos>>(itrBCI,BCI_positions.end());
-      //std::cerr << __LINE__<<"\n";
+      std::cerr << __LINE__<<"\n";
       itrBCI=BCI_positions.end()-1;
-      //std::cerr << __LINE__<<"\n";
+      std::cerr << __LINE__<<"\n";
       ret =  pthread_create(&list_thread[thread], &attr, &fillList, &kmer_list_structs[thread]);
       if(ret != 0) {
         printf("Error: pthread_create() failed\n");
@@ -497,7 +497,7 @@ while (std::getline(file1,meta)) { // proceeding through files
       // std::cerr << "starting thread " << (int)thread << "\n";
       // active_threads[thread]=true;
       thread=(thread+1)%(thread_count);
-      //std::cerr << __LINE__<<"\n";
+      std::cerr << __LINE__<<"\n";
 
       pthread_join(list_thread[thread],NULL);
       // kmer_list_structs[thread].reads.push_back({});
