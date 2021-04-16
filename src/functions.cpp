190512c0686e7a89fd44d5cc7e44ad24ba5b0359
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-04-16 13:19:40 +0200

    Add files via upload

--- src/functions.cpp
+++ src/functions.cpp
@@ -25,7 +25,7 @@ std::vector<Dna5String> GetReads(std::pair<std::streampos,std::streampos> & BCI_
     //std::cerr << __LINE__ << "\n";
     readRecord(id,read1,file1);
     // std::cerr << __LINE__ << "\n";
-    std::cerr << file1.stream.file.tellg() << "\n";
+    // std::cerr << file1.stream.file.tellg() << "\n";
     readRecord(id,read2,file2);
     // std::cerr << __LINE__ << "\n";
     reads.push_back(read1);
