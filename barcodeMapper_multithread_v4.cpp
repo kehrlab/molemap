f193963fc3b281c793f9e08984ff045bb11a27b5
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-04-16 07:18:27 +0200

    Add files via upload

--- barcodeMapper_multithread_v4.cpp
+++ barcodeMapper_multithread_v4.cpp
@@ -467,7 +467,7 @@ BCI_positions.push_back(std::make_pair(BCI_pos1,BCI_pos2));
 itrBCI=BCI_positions.begin();
 std::cerr << __LINE__<<"\n";
 std::cerr << "BCI_pos1: " << (int)std::get<0>(*itrBCI)<< "\n";
-std::cerr << "BCI_pos1: " << (int)std::get<0>(BCI_positions.begin())<< "\n";
+std::cerr << "BCI_pos1: " << (int)std::get<0>(*BCI_positions.begin())<< "\n";
 
 while (std::getline(file1,meta)) { // proceeding through files
   file1.ignore(10000,'\n');
