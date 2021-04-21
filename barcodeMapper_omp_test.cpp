e1eb1ae11851e2ccc05c42f57b0a191fb3fffe36
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-04-21 09:05:33 +0200

    Add files via upload

--- barcodeMapper_omp_test.cpp
+++ barcodeMapper_omp_test.cpp
@@ -425,9 +425,11 @@ while (!atEnd(file1)){ // reading and processing next batch of reads until file 
     }
 
     // if (i==2){   // process reads and write results to file
-    #pragma omp for
+    #pragma omp section
+    {
       // itrbarc=barcodeSet[thread3].begin();
       // #pragma omp parallel for
+      #pragma omp parallel for
       for (itrreadSet = readSet[thread3].begin(), itrbarc=barcodeSet[thread3].begin(); itrreadSet != readSet[thread3].end(); itrreadSet++ ,itrbarc++) {// for all barcodes in set
         // std::cerr << __LINE__ << "\n";
         std::vector<std::tuple<uint_fast8_t,uint32_t,uint32_t,uint32_t>> kmer_list;   // (i,j,a,m_a)   i=reference (Chromosome), j=position of matching k-mer in reference, a=abundance of k-mer in reference, m_a=minimizer_active_bases
@@ -488,7 +490,7 @@ while (!atEnd(file1)){ // reading and processing next batch of reads until file 
       omp_unset_lock(&lock);
       thread3=(thread3+1)%3;
       // std::cerr << __LINE__ << "\n";
-    // } //if (i==2)
+    } //if (i==2)
   }
   // std::cerr << __LINE__ << "!!!!!!!!!!!!!\n";
 }   //pragma omp parallel
