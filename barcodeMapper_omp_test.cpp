16651a18ade08649eaa738c014ec6af132badc25
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-04-21 14:07:40 +0200

    Add files via upload

--- barcodeMapper_omp_test.cpp
+++ barcodeMapper_omp_test.cpp
@@ -364,7 +364,7 @@ for(int i=0;i<2;i++){
 
 while (!atEnd(file1)){ // reading and processing next batch of reads until file endpos
 
-      // std::cerr << __LINE__ << "\n";
+      std::cerr << __LINE__ << "\n";
 
 
       auto tbegin3 = std::chrono::high_resolution_clock::now();
@@ -378,7 +378,7 @@ while (!atEnd(file1)){ // reading and processing next batch of reads until file 
       for (int i=-2; i<(int)barcodeSet[thread3].size(); i++) {// for all barcodes in set
         if (i==-2){       // read from file 1
           auto tbegin = std::chrono::high_resolution_clock::now();
-          // std::cerr << __LINE__ << "\n";
+          std::cerr << __LINE__ << "\n";
           omp_set_lock(&lock);
           while (!atEnd(file1)){
             BCI_pos1=file1.stream.file.tellg();
@@ -411,7 +411,7 @@ while (!atEnd(file1)){ // reading and processing next batch of reads until file 
         }
         else if(i==-1){   // read from file 2
           auto tbegin2 = std::chrono::high_resolution_clock::now();
-          // std::cerr << __LINE__ << "\n";
+          std::cerr << __LINE__ << "\n";
           for (uint32_t barc=0; barc<barcodeSet[thread2].size(); barc++){
             uint32_t r_count=readSet[thread2][barc].size();
             BCI_pos2=file2.stream.file.tellg();
