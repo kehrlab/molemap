9c9ddff44ab156ea2295737d9a1644a4c8c70c5f
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-05-19 09:24:50 +0200

    Add files via upload

--- barcodeMapper_omp_v3.cpp
+++ barcodeMapper_omp_v3.cpp
@@ -327,6 +327,7 @@ int main(int argc, char const ** argv){
       barcode=skipToNextBarcode(file1, id1);
       itrwhitelist=std::lower_bound(whitelist.begin(), whitelist.end(), barcode); //position of first bc in whitelist that is not smaler than barcode
     }
+    std::cerr << "barcode: " << barcode << " whitelist: " << *itrwhitelist << " thread: " << t << "\n";
     if(atEnd(file1)){
       std::cerr << "I will continue!\n";
       continue;
