d78df6ff62361de16f228ccf812226f2aadadb2c
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-05-28 15:08:35 +0200

    Add files via upload

---
+++ bcmap.cpp
@@ -0,0 +1,27 @@
+# include <seqan/seq_io.h>
+# include <seqan/sequence.h>
+# include <seqan/arg_parse.h>
+# include <iostream>
+# include <fstream>
+# include "./src/functions.h"
+# include "./src/index.h"
+# include "./src/get.h"
+# include "./src/map.h"
+# include <time.h>
+using namespace seqan;
+
+int main(int argc, char const ** argv){
+  std::string arg1(argv[1]);
+  --argc;
+  argv++;
+  if(arg1=="index"){
+    index(argc, argv);
+  }
+  else if(arg1=="map"){
+    map(argc, argv);
+  }
+  else if(arg1=="get"){
+    get(argc, argv);
+  }
+
+}
