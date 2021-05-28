c0bfe25bc16e9bef3df0bb65c6c0aa56db68c502
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-05-28 15:23:29 +0200

    Add files via upload

--- bcmap.cpp
+++ bcmap.cpp
@@ -11,13 +11,17 @@
 using namespace seqan;
 
 int main(int argc, char const ** argv){
+
+  if (argc==0){
+    std::cerr << "\nNo command!\nCommands are: index, map, get.\nUse './bcmap [command] --help' for more information.\n";
+    return;
+  }
+
   std::string arg1(argv[1]);
   --argc;
   argv++;
-  if (argc==0){
-    std::cerr << "\nNo command!\nCommands are: index, map, get.\nUse './bcmap [command] --help' for more information.\n";
-  }
-  else if(arg1=="index"){
+
+  if(arg1=="index"){
     index(argc, argv);
   }
   else if(arg1=="map"){
