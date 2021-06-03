5a79e685ff69963d93cd4b4e3763f66b2ead7ff7
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-06-03 12:58:34 +0200

    Add files via upload

--- testing.py
+++ testing.py
@@ -5,16 +5,24 @@ def getbarcode(line):
     print(barcode)
     return barcode
 
+def getmapping(line):
+    mapping=line.split('\t')[0:2]
+    pring(mapping)
+    return mapping
+
+
 bcmap_res=open('resall.bed','r')
 # bwa_res=open('','r')
 # readfile=open('159916111600 25. Feb 14:36 NA12878_WGS_v2_S1_L001_all_corrected.1.fastq','r')
 
 old_barcode=''
-
+mappings=[[]]
 for line in bcmap_res:
     barcode=getbarcode(line)
-    # if barcode==old_barcode
+    if barcode==old_barcode:
+        mappings+=getmapping(line)
         #append mappings
 
-    # else
+    else:
+        mappings=[[]]
         #evaluate and create new mappings
