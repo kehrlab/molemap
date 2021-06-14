4da159c4e3161ec4d9aa2d6bd68fa389febeecef
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-06-14 15:17:59 +0200

    Add files via upload

--- testing.py
+++ testing.py
@@ -27,14 +27,16 @@ def getmapping(line):
 def evaluate(bwa_line, mappings):
     bwamap=bwa_line.split('\t')
     give=20000
+    res=-1
     for mapping in mappings:
+        res+=1
         if mapping[0]==bwamap[2]:
             if (int(bwamap[3])>int(mapping[1])-give and int(bwamap[3])<int(mapping[2])+give and int(bwamap[7])>int(mapping[1])-give and int(bwamap[7])<int(mapping[2])+give):
                 # print("mapping: ",mapping,"\n")
-                return 1
+                return res
 
     # print("bwamap: ",bwamap,"\n")
-    return 0
+    return -1
 # parameters:
 tp_per=0.5 #fraction of reads that have to be bwa_mapped to an bcmap_identified position to count BC as TP
 
@@ -71,8 +73,9 @@ while tenXbc=='*':
 
 # readcount=0
 barcodecount=0
-tp=0
+TP=0
 FN=0
+FP=0
 old_barcode=getbarcode(bcmap_res.readline())
 mappings=[]
 for line in bcmap_res:
@@ -82,6 +85,7 @@ for line in bcmap_res:
         mappings+=[getmapping(line)]
 
     else:
+        mapping_list=[0]*len(mapping)
         barcodecount+=1
         #evaluate
         correct=0
@@ -107,7 +111,9 @@ for line in bcmap_res:
         while tenXbc==old_barcode:
             # bwa_line=bwa_res.readline()
             while getbwaID(bwa_line)==tenXid:
-                correct+=evaluate(bwa_line, mappings)
+                res=evaluate(bwa_line, mappings)
+                if res!=-1:
+                    mapping_list[res]+=1
                 reads+=1
                 bwa_line=bwa_res.readline()
             readfileline=readfile.readline()
@@ -131,9 +137,9 @@ for line in bcmap_res:
         # print(mappings,"\n")
         # print("reads:   ", reads, "\n")
         # print("correct: ", correct, "\n\n")
-        if correct/reads<tp_per:
+        if sum(mapping_list)/reads<tp_per:
             FN+=1
-        print("FN: " , round(FN/barcodecount*100,2))
+        print("FN: " , round(FN/barcodecount*100,2),"% ")
         old_barcode=barcode
         mappings=[[]]
         mappings[0]=getmapping(line)
