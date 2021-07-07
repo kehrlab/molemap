0dac8c1eb83ee2fb613e3b569a3db01d86395d1f
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-07-07 13:00:03 +0200

    Add files via upload

--- src/map.cpp
+++ src/map.cpp
@@ -327,6 +327,7 @@ int map(int argc, char const ** argv){
       file1.stream.file.seekg(startpos);
       barcode=skipToNextBarcode(file1, id1);
     } else {
+      std::cerr << file1.stream.file.getline() << "\n";
       std::cerr << __LINE__ << "\n";
       readRecord(id1, read1, file1);
       std::cerr << __LINE__ << "\n";
