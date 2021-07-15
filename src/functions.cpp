91e45f8f20622c7c014605d65a14315e0d4f2c6f
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-07-15 12:34:34 +0200

    Add files via upload

--- src/functions.cpp
+++ src/functions.cpp
@@ -8,7 +8,11 @@ using namespace seqan;
 
 //retreive the barcode from 10x linked reads
 std::string get10xBarcode(std::string id1){
+  std::cerr << id1 << "\n";
+  std::cerr << id1.find(" ");
+  std::cerr << __LINE__ << "\n";
   id1=id1.substr(id1.find(" "),10000);
+  std::cerr << __LINE__ << "\n";
   std::string new_barcode=id1.substr(id1.find("BX:Z:")+5,16);
   return new_barcode;
 }
