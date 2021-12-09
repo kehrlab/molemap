7134d8f3ef49b299b18c63066009dc8da2640200
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-12-09 14:37:17 +0100

    Plot score distribution.

---
+++ plot_score_histogram.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import sys
+
+if len(sys.argv)!=2:
+    print("usage: python plot_score_histogram.py bcmap_output.hist")
+
+hist_file=open(sys.argv[1],"r")
+freq=[int(line) for line in hist_file]
+scores=[i for i in range(200)]
+
+plt.bar(scores,freq,color='#7070c0')
+plt.xlabel("score")
+plt.ylabel("frequency")
+plt.title("Score Histogram for "+sys.argv[1][:-5])
+plt.show()
