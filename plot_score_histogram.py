b263ce3b9b2dafa6b7a298be30004b6825e577fd
Richard-Luepken <63794504+Richard-Luepken@users.noreply.github.com>
2021-12-16 13:39:23 +0100

    Bugfix

--- plot_score_histogram.py
+++ plot_score_histogram.py
@@ -3,7 +3,8 @@ import sys
 
 if len(sys.argv)!=2:
     print("usage: python plot_score_histogram.py bcmap_output.hist")
-
+    quit()
+    
 hist_file=open(sys.argv[1],"r")
 freq=[int(line) for line in hist_file]
 scores=[i for i in range(200)]
@@ -11,5 +12,5 @@ scores=[i for i in range(200)]
 plt.bar(scores,freq,color='#7070c0')
 plt.xlabel("score")
 plt.ylabel("frequency")
-plt.title("Score Histogram for "+sys.argv[1][:-5])
+plt.title("Score Histogram")# for "+sys.argv[1][:-5])
 plt.show()
